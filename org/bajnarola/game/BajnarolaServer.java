37484d33faae9fc09ac7ee4f01e4df2403b73c0a
Davide Berardi <berardi.dav@gmail.com>
2015-09-17 17:49:58 +0000

    Skeleton

---
+++ org/bajnarola/game/BajnarolaServer.java
@@ -0,0 +1,38 @@
+/*****************************************************************************/
+/* Server package for Bajnarola distributed game                             */
+/*                                                                           */
+/* Copyright (C) 2015                                                        */
+/* Marco Melletti, Davide Berardi, Matteo Martelli                           */
+/*                                                                           */
+/* This program is free software; you can redistribute it and/or             */
+/* modify it under the terms of the GNU General Public License               */
+/* as published by the Free Software Foundation; either version 2            */
+/* of the License, or any later version.                                     */
+/*                                                                           */
+/* This program is distributed in the hope that it will be useful,           */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of            */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             */
+/* GNU General Public License for more details.                              */
+/*                                                                           */
+/* You should have received a copy of the GNU General Public License         */
+/* along with this program; if not, write to the Free Software               */
+/* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,*/
+/* USA.                                                                      */
+/*****************************************************************************/
+
+package org.bajnarola.game;
+import java.rmi.Naming;
+
+public class BajnarolaServer {
+	public static void main(String[] argv) {
+		try {
+			BoardImpl b1 = new BoardImpl(10);
+			
+			Naming.rebind("rmi://localhost/Board1", b1);
+			
+			b1.points = 24;
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+}
