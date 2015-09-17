4bd5b594019b5336bc32f944eede2cc2f7f317e0
Marco Melletti <mellotanica@hotmail.it>
2015-09-17 18:46:30 +0200

    package

---
+++ org/bajnarola/game/BoardImpl.java
@@ -0,0 +1,40 @@
+/*****************************************************************************/
+/* Local board implementation class                                          */
+/*                                                                           */
+/* Copyright (C) 2015                                                        */
+/* Marco Melletti, Davide Berardi, Matteo Martelli                           */
+/*                                                                           */
+/* This program is free software; you can redistribute it and/or             */
+/* modify it under the terms of the GNU General Public License               */
+/* as published by the Free Software Foundation; either version 2            */
+/* of the License, or any later version.                                     */
+/*                                                                           */
+/* This program is distributed in the hope that it will be useful,           */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of            */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             */
+/* GNU General Public License for more details.                              */
+/*                                                                           */
+/* You should have received a copy of the GNU General Public License         */
+/* along with this program; if not, write to the Free Software               */
+/* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,*/
+/* USA.                                                                      */
+/*****************************************************************************/
+
+package org.bajnarola.game;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class BoardImpl extends UnicastRemoteObject implements Board {
+	private static final long serialVersionUID = -6564070861147997471L;
+
+
+	int points;
+	
+	public BoardImpl(int points) throws RemoteException {
+		this.points = points;
+	}
+
+	public Integer getPoints() {
+		return this.points;
+	}
+}
