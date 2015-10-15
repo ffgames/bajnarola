1dcf6a75ec66ad6b3e3acbe4ed35d9f0f4d4b709
Marco Melletti <mellotanica@hotmail.it>
2015-10-15 12:53:33 +0200

    gt

--- org/bajnarola/game/view/GraphicalTile.java
+++ org/bajnarola/game/view/GraphicalTile.java
@@ -1,10 +1,11 @@
 package org.bajnarola.game.view;
 
-import org.bajnarola.game.model.Tile;
-
 public class GraphicalTile {
 	String fname;
-	Tile tile;
 	
 	static final String GTILE_EXTENSION = ".jpg";
+	
+	public GraphicalTile(String name){
+		this.fname = name + GTILE_EXTENSION;
+	}
 }
