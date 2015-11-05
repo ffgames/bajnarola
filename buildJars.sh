551a962b76a717880a1ed3f7f05ea71e78b77eb0
Marco Melletti <mellotanica@hotmail.it>
2015-11-05 17:58:50 +0100

    Feed. You. Stuff. No time.

--- buildJars.sh
+++ buildJars.sh
@@ -3,7 +3,7 @@
 JVERS=`java -version 2>&1 | head -n 1 | cut -d "\"" -f 2 | cut -d "_" -f 1`
 DEST_DIR="build/java_$JVERS"
 
-jar cfm lobby.jar lobbyManifest.txt org/bajnarola/lobby/LobbyServer.class org/bajnarola/lobby/LobbyServer_Stub.class org/bajnarola/lobby/LobbyController.class org/bajnarola/networking/NetPlayer.class
+jar cfm lobby.jar lobbyManifest.txt org/bajnarola/lobby/LobbyServer.class org/bajnarola/lobby/LobbyServer_Stub.class org/bajnarola/lobby/LobbyController.class org/bajnarola/networking/NetPlayer.class org/bajnarola/utils/BajnarolaRegistry.class
 
 #compile game
 
