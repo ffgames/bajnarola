78bb6a3f13226210578030d70f4d79b8859646dc
matteomartelli <matteomartelli3@gmail.com>
2015-11-06 19:22:08 +0100

    mhm well

--- build.sh
+++ build.sh
@@ -2,13 +2,18 @@
 
 JVERS=`java -version 2>&1 | head -n 1 | cut -d "\"" -f 2 | cut -d "_" -f 1`
 DEST_DIR="build/java_$JVERS"
-#TODO compile lobby
+#Compile everything
+ant
+rmic org.bajnarola.game.controller.GameController \
+     org.bajnarola.game.BajnarolaServer \
+     org.bajnarola.lobby.LobbyServer
+
+
 #Pack Lobby Server
 jar cfe lobby.jar org.bajnarola.lobby.LobbyServer org/bajnarola/lobby/LobbyServer.class \
 		org/bajnarola/lobby/LobbyServer_Stub.class org/bajnarola/lobby/LobbyController.class \
 		org/bajnarola/networking/NetPlayer.class org/bajnarola/utils/BajnarolaRegistry.class
 
-#TODO compile game
 #Pack game
 jar cfm game.jar gameManifest.txt org/bajnarola/game/*.class org/bajnarola/game/*/*.class org/bajnarola/networking/*.class org/bajnarola/utils/*.class org/bajnarola/lobby/*.class lib/* -J-Djava.library.path=lib/native:lib/native -J-Dfile.encoding=UTF-8 
 mkdir -p "$DEST_DIR"
